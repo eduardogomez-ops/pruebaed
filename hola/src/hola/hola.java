1ea6c01d5e420469d56ed029016c12a7933429eb
eduar <eduar@usuario>
2026-04-29 17:26:16 +0200

    Prueba 1

---
+++ hola/src/hola/hola.java
@@ -0,0 +1,14 @@
+package hola;
+
+public class hola {
+
+	public hola() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola");
+	}
+
+}
